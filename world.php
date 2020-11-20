8ca7615229dbced59731c8a4b3bf6cb472b88583
Marlon Williams <marlon.kingson@gmail.com>
2020-11-20 11:50:26 -0500

    Edited Php File

--- world.php
+++ world.php
@@ -1,17 +1,19 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+#$stmt = $conn->query("SELECT * FROM countries");
+$stmt1 = $conn->prepare("SELECT * FROM countries WHERE name = :country");
+$country = $_GET["country"];
+$stmt1->bindParam(':country',$country);
+$stmt1->execute();
 
+#$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+while ($row = $stmt1->fetch(PDO::FETCH_ASSOC)){
+  echo $row['name'] . ' is ruled by ' . $row['head_of_state'];
+}
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
